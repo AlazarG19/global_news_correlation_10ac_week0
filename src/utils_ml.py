f88c7490c7154c1a3b13270246349b44bee5cb82
AlazarG19 <alazargetachew70@gmail.com>
2024-08-28 22:53:27 +0300

    feat: added funcitons in utils to aid in keyword extraction, performed keyword extraction using tfidf and classified topics into predetermined topics

--- src/utils_ml.py
+++ src/utils_ml.py
@@ -32,3 +32,70 @@ def extract_similarity(df,feature_1,feature_2,new_feature,ngram_range=(1,2),min_
         sim = cosine_similarity(tf_feature_1_feature_2_matrix, tf_feature_1_feature_2_matrix)
         
         df.loc[index, new_feature] = sim[0][1] 
+        
+def sort_coo(coo_matrix):
+    """Sort a dict with highest score"""
+    tuples = zip(coo_matrix.col, coo_matrix.data)
+    return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
+
+def extract_topn_from_vector(feature_names, sorted_items, topn=10):
+    """get the feature names and tf-idf score of top n items"""
+    
+    #use only topn items from vector
+    sorted_items = sorted_items[:topn]
+
+    score_vals = []
+    feature_vals = []
+    
+    
+    # word index and corresponding tf-idf score
+    for idx, score in sorted_items:
+        
+        #keep track of feature name and its corresponding score
+        score_vals.append(round(score, 3))
+        feature_vals.append(feature_names[idx])
+
+    #create a tuples of feature, score
+    results= {}
+    for idx in range(len(feature_vals)):
+        results[feature_vals[idx]]=score_vals[idx]
+    
+    return results
+
+def get_keywords(vectorizer, feature_names, docs, TOP_K_KEYWORDS):
+    """Return top k keywords for each doc in the corpus using TF-IDF method"""
+    tf_idf_matrix = vectorizer.transform(docs)
+    sorted_items = [sort_coo(tf_idf_vector.tocoo()) for tf_idf_vector in tf_idf_matrix]
+    keywords = [extract_topn_from_vector(feature_names, sorted_items[i], TOP_K_KEYWORDS) for i in range(len(docs))]
+    return keywords
+
+def key_word_extractor(data):
+    """ Fetching the dataframe ,generating the keywords column and returning it as a list"""
+    vectorizer = TfidfVectorizer(smooth_idf=True, use_idf=True)
+    corpora = data.to_list()
+    vectorizer.fit_transform(corpora)
+    feature_names = vectorizer.get_feature_names_out()
+
+    keywords = get_keywords(vectorizer, feature_names, corpora, 10)
+    result = [{'top_keywords': list(keywords[i].keys())} for i, doc in enumerate(corpora)]
+    final = pd.DataFrame(result)
+    return final
+
+def categorize_headlines(headlines, tags):
+    """ Function to categorize the headlines into tags """
+    categories = []
+    for headline in headlines:
+        headline = headline.lower()
+        headline_tags = []
+
+
+        for tag, keywords in tags.items():
+            if any(keyword in headline for keyword in keywords):
+                headline_tags.append(tag)
+
+        if not headline_tags:
+            headline_tags.append("Other")
+
+        categories.append(', '.join(headline_tags))
+
+    return categories
