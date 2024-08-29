622e5ba99cb19b412c2f19467fbf34542b63fd2a
AlazarG19 <alazargetachew70@gmail.com>
2024-08-29 17:51:33 +0300

    feat: adding comment to utils_ml, adding topic modelling function to utils_ml and creating topic modeling using tfidf and lda model

--- src/utils_ml.py
+++ src/utils_ml.py
@@ -1,7 +1,9 @@
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import TruncatedSVD, LatentDirichletAllocation
+
 import string
 import pandas as pd
 
@@ -128,7 +130,19 @@ def get_keywords(vectorizer, feature_names, docs, TOP_K_KEYWORDS):
     return keywords
 
 def key_word_extractor(data):
-    """ Fetching the dataframe ,generating the keywords column and returning it as a list"""
+    """
+    Extracts top keywords from a given dataframe.
+
+    Parameters:
+    data (pandas.DataFrame): Input dataframe containing text data.
+
+    Returns:
+    pandas.DataFrame: A dataframe with a single column 'top_keywords' containing a list of top keywords for each document.
+
+    Notes:
+    - The function uses TF-IDF vectorization to generate keywords.
+    - The number of top keywords to extract is set to 10, but can be adjusted by modifying the `get_keywords` function.
+    """
     vectorizer = TfidfVectorizer(smooth_idf=True, use_idf=True)
     corpora = data.to_list()
     vectorizer.fit_transform(corpora)
@@ -140,12 +154,23 @@ def key_word_extractor(data):
     return final
 
 def categorize_headlines(headlines, tags):
-    """ Function to categorize the headlines into tags """
+    """
+    Categorize headlines into tags based on keyword matching.
+
+    Parameters:
+    headlines (list[str]): A list of headlines to be categorized.
+    tags (dict[str, list[str]]): A dictionary where each key is a tag and each value is a list of keywords associated with that tag.
+
+    Returns:
+    list[str]: A list of categorized headlines, where each headline is a comma-separated string of tags.
+
+    Notes:
+    If a headline does not match any keywords, it will be categorized as "Other".
+    """
     categories = []
     for headline in headlines:
         headline = headline.lower()
         headline_tags = []
-
 
         for tag, keywords in tags.items():
             if any(keyword in headline for keyword in keywords):
@@ -158,10 +183,27 @@ def categorize_headlines(headlines, tags):
 
     return categories
 
-def topic_modelling_tfidf(content,max_features=1000, max_df = 0.5,n_components=10,n_iter=100,random_state=122):
-    topic_tfidf = TfidfVectorizer( max_features= max_features, max_df =  max_df , smooth_idf=True)
+def topic_modelling_tfidf(content, max_features=1000, max_df=0.5, n_components=10, n_iter=100, random_state=122):
+    """
+    Perform topic modeling using TF-IDF vectorization and truncated SVD.
+
+    Parameters:
+    content (list of str): The list of text documents to perform topic modeling on.
+    max_features (int, optional): The maximum number of features to extract from the text data. Defaults to 1000.
+    max_df (float, optional): The maximum document frequency for features to be considered. Defaults to 0.5.
+    n_components (int, optional): The number of topics to extract. Defaults to 10.
+    n_iter (int, optional): The number of iterations for the truncated SVD algorithm. Defaults to 100.
+    random_state (int, optional): The random seed for reproducibility. Defaults to 122.
+
+    Returns:
+    dict: A dictionary where each key is a topic label (e.g. "Topic 0") and each value is a list of the top 7 terms associated with that topic.
+
+    Notes:
+    This function uses the TfidfVectorizer to convert the text data into a TF-IDF matrix, and then applies truncated SVD to reduce the dimensionality of the matrix and extract the topics.
+    """
+    topic_tfidf = TfidfVectorizer(max_features=max_features, max_df=max_df, smooth_idf=True)
     topic_vector = topic_tfidf.fit_transform(content)
-    topic_vector_array= topic_vector.toarray()
+    topic_vector_array = topic_vector.toarray()
     svd_model = TruncatedSVD(n_components=n_components, algorithm='randomized', n_iter=n_iter, random_state=random_state)
     svd_model.fit(topic_vector_array)
     topic_vector_array_reduced = svd_model.transform(topic_vector_array)
@@ -171,10 +213,43 @@ def topic_modelling_tfidf(content,max_features=1000, max_df = 0.5,n_components=1
 
     for i, comp in enumerate(svd_model.components_):
         terms_comp = zip(terms, comp)
-        sorted_terms = sorted(terms_comp, key= lambda x:x[1], reverse=True)[:7]
+        sorted_terms = sorted(terms_comp, key=lambda x: x[1], reverse=True)[:7]
         topics_list = []
         for t in sorted_terms:
             topics_list.append(t[0])
-        topics["Topic "+str(i)] = topics_list
+        topics["Topic " + str(i)] = topics_list
     return topics
+
+def topic_modelling_lda(data, num_topics, min_df=10, token_pattern='[a-zA-Z0-9]{3,}', max_features=50000, max_iter=10, batch_size=128):
+    """
+    Perform topic modeling using Latent Dirichlet Allocation (LDA) on a given dataset.
+
+    Parameters:
+    - data (list of str): List of documents to perform topic modeling on.
+    - num_topics (int): Number of topics to extract from the data.
+    - min_df (int, optional): Minimum frequency of words to be considered. Defaults to 10.
+    - token_pattern (str, optional): Regular expression pattern to tokenize words. Defaults to '[a-zA-Z0-9]{3,}'.
+    - max_features (int, optional): Maximum number of features (words) to extract. Defaults to 50000.
+    - max_iter (int, optional): Maximum number of iterations for the LDA model. Defaults to 10.
+    - batch_size (int, optional): Batch size for the LDA model. Defaults to 128.
+
+    Returns:
+    - lda_output (array): The Trained LDA Model.
+    - lda_output (array): Document-topic matrix where each row represents a document and each column represents a topic.
+    - vectorizer: Countvectorizer used to create features for lsa.
+    - output (dict): Dictionary where each key is a topic and the value is a list of the top 10 words associated with that topic.
+
+    Notes:
+    - This function uses the CountVectorizer from scikit-learn to convert the text data into a numerical representation.
+    - The LDA model is then applied to this representation to extract the topics.
+    """
+    vectorizer = CountVectorizer(analyzer='word', min_df=min_df, lowercase=True, token_pattern=token_pattern, max_features=max_features)
+    vectorized_feature = vectorizer.fit_transform(data)
+    # Create the LDA model
+    lda_model = LatentDirichletAllocation(n_components=num_topics, max_iter=max_iter, learning_method='online', random_state=100, batch_size=batch_size, evaluate_every=-1, n_jobs=-1)
+    lda_output = lda_model.fit_transform(vectorized_feature)
+    output = {}
+    for index, topic in enumerate(lda_model.components_):
+        output[f"Topic {index}"] = [vectorizer.get_feature_names_out()[i] for i in topic.argsort()[-10:]]
+    return lda_model, lda_output,vectorizer, output
 
